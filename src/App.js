046b45c38f711223065f4824eec657cc68aa596a
Quinten97 <89324378+Quinten97@users.noreply.github.com>
2022-01-14 08:28:58 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import GetJoke from "./getJoke";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <GetJoke />
+    </div>
+  );
+}
---
+++ src/getJoke.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export default function GetJoke() {
+  const [data, setData] = useState(null);
+
+  let url = "https://v2.jokeapi.dev/joke/Any?safe-mode";
+
+  const loadData = async () => {
+    return await fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  };
+
+  const sortData = () => {
+    if (data.type === "twopart") {
+      return ` ${data.setup} ${data.delivery}`;
+    } else {
+      return data.joke;
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div className="master-container">
+      <div className="text-container">{data ? sortData() : ""}</div>
+      <button
+        className="btn"
+        onClick={() => {
+          loadData();
+        }}
+      >
+        New Joke
+      </button>
+    </div>
+  );
+}
